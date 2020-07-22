95fe026c595316a75a127247222961dd4a506f07
Wesley <wesaboo@gmail.com>
2020-07-21 19:46:51 -0700

    modal button

--- public/js/members.js
+++ public/js/members.js
@@ -2,7 +2,7 @@
 const modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+const btn = document.getElementById("cardSearch");
 
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
